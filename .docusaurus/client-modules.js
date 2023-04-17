81f0e2fd0db75bb19aa34de6066318a3403ac87d
alaihsan <mail.ichsan24@gmail.com>
2023-04-17 17:37:08 +0700

    fresh install docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/ihsan/dev/tanse-rules-alsen22/node_modules/infima/dist/css/default/default.css'),
+  require('/home/ihsan/dev/tanse-rules-alsen22/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/ihsan/dev/tanse-rules-alsen22/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/ihsan/dev/tanse-rules-alsen22/src/css/custom.css'),
+];
